786050a8d06074f1fa658f9306d8d4ca23c5517d
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-21 16:53:32 +0100

    Add route from react-router

---
+++ client/components/movie-create.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class MovieCreate extends Component {
+  render() {
+    return <div>Movie create</div>;
+  }
+}
+export default MovieCreate;
--- client/index.js
+++ client/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import MovieList from './components/movie-list';
+import MovieCreate from './components/movie-create';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <MovieList />
+      <Router history={hashHistory}>
+        <Route path='/'>
+          <IndexRedirect to='/movies' />
+          <Route path='/movies' component={MovieList} />
+          <Route path='/movies/create' component={MovieCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
