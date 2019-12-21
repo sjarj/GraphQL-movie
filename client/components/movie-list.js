5a56d22b1c20ba6e47e6e8ee14f9c3cc280ec608
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-21 16:04:24 +0100

    Request with graphQL tag

--- client/components/movie-list.js
+++ client/components/movie-list.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class MovieList extends Component {
+  rendrMovies() {
+    if (!this.props.data.loading) {
+      return this.props.data.movies.map(movie => {
+        return <li key={movie.id}>{movie.title}</li>;
+      });
+    } else {
+      return <div>Loading....</div>;
+    }
+  }
+
   render() {
-    return <div>List de films</div>;
+    return <ul>{this.rendrMovies()}</ul>;
   }
 }
+const query = gql`
+  {
+    movies {
+      id
+      title
+    }
+  }
+`;
 
-export default MovieList;
+export default graphql(query)(MovieList);
