9145dae64dd3b0ba3dd3fcb95199c7548da34db4
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-21 17:29:33 +0100

    Form  movie create

--- client/components/movie-create.js
+++ client/components/movie-create.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class MovieCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { terms: '' };
+  }
+
   render() {
-    return <div>Movie create</div>;
+    return (
+      <div>
+        <h1>Ajouter un film</h1>
+        <form className='col s6 input-field'>
+          <input
+            type='text'
+            className='validate'
+            onChange={e => this.setState({ terms: e.target.value })}
+          />
+          <label className='active'>Titre</label>
+        </form>
+      </div>
+    );
   }
 }
 export default MovieCreate;
