c271be7582e88af6a190514257128f139b6b1e66
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-21 15:05:55 +0100

    connect to mongodb

--- server/server.js
+++ server/server.js
@@ -8,25 +8,28 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Remplacer avec ton url mlab
-const MONGO_URI = 'mongodb://robinuser:robinuser@ds245347.mlab.com:45347/dbrobin001';
+const MONGO_URI =
+  'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('Tu dois fournir une url mongoDB');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI, 
-{
-  useMongoClient:true
+mongoose.connect(MONGO_URI, {
+  useMongoClient: true
 });
 mongoose.connection
-    .once('open', () => console.log('Connecté à MongoLab'))
-    .on('error', error => console.log('Erreur de connexion à MongoLab:', error));
+  .once('open', () => console.log('Connecté à MongoLab'))
+  .on('error', error => console.log('Erreur de connexion à MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true,
-}));
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
